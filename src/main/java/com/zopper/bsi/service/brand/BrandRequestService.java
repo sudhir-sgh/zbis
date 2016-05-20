e17034c23ea0b3dab5ef7dad5b3d3f363d07c73f
akankshazopper <akankshagupta.dec.12@gmail.com>
2016-05-20 16:01:59 +0530

    Servivce Pattern..

---
+++ src/main/java/com/zopper/bsi/service/brand/BrandRequestService.java
@@ -0,0 +1,11 @@
+package com.zopper.bsi.service.brand;
+/**
+ * Created by sudhir on 20/5/16.
+ */
+public interface BrandRequestService {
+	
+	public String raiseDemoRequest();
+	
+	public String raiseInstallationRequest();
+
+}
