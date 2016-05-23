e5ff28a9355ccc8ee02223e7645607e3c6c97fc1
Tarun Kumar <tarunkumar88@gmail.com>
2016-05-23 20:16:31 +0530

    autowired entity manager

--- src/main/java/com/zopper/bsi/dao/impl/ServiceRequestDaoImpl.java
+++ src/main/java/com/zopper/bsi/dao/impl/ServiceRequestDaoImpl.java
@@ -2,12 +2,10 @@ package com.zopper.bsi.dao.impl;
 
 import java.util.List;
 
-import org.hibernate.HibernateException;
-import org.hibernate.Query;
-import org.hibernate.Session;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.orm.hibernate4.HibernateCallback;
-import org.springframework.orm.hibernate4.HibernateTemplate;
 
 import com.zopper.bsi.dao.ServiceRequestDaoCustom;
 import com.zopper.bsi.models.ServiceRequest;
@@ -15,22 +13,15 @@ import com.zopper.bsi.request.Criteria;
 
 public class ServiceRequestDaoImpl implements ServiceRequestDaoCustom {
 	
-	//private @Autowired HibernateTemplate hibernateTemplate;
+	private @Autowired EntityManager entityManager;
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public List<ServiceRequest> getAllServiceRequests(Criteria criteria) {
 		System.out.println("\n\n**********ServiceRequestDaoImpl.getAllServiceRequests() called**********\n\n");
-		/*return hibernateTemplate.execute(new HibernateCallback<List<ServiceRequest>>() {
-
-			@SuppressWarnings("unchecked")
-			@Override
-			public List<ServiceRequest> doInHibernate(Session session) throws HibernateException {
-				String hqlQuery = "FROM ServiceRequest ";
-				Query query = session.createQuery(hqlQuery);
-				return query.list();
-			}
-		});*/
-		return null;
+		String hqlQuery = "FROM ServiceRequest ";
+		Query query = entityManager.createQuery(hqlQuery);
+		return query.getResultList();
 	}
 
 }
